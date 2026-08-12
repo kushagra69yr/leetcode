8c178f67e57cf0167537b5b3ed9edca405dbda45
Kushagra Burman <kushagraburman21@gmail.com>
2026-08-12 23:46:33 +0530

    Time: 4 ms (79.55%), Space: 13.6 MB (49.94%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,8 +1,9 @@
-class Solution(object):
-    def moveZeroes(self, nums):
+class Solution:
+    def moveZeroes(self,nums):
         k=0
+
         for i in range(len(nums)):
-            if nums[i]!=0:
-                nums[k],nums[i] =nums[i],nums[k]
+            if nums[i] !=0:
+                nums[k],nums[i] = nums[i],nums[k]
                 k+=1
         
