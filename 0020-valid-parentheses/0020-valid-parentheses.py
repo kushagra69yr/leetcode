67393fb9503dce95cdd06accdab6bc4adf09caf5
Kushagra Burman <kushagraburman21@gmail.com>
2026-08-13 16:23:29 +0530

    Time: 1 ms (86.18%), Space: 12.6 MB (10.27%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s):
+        st = []
+        m = {')':'(', ']':'[', '}':'{'}
+
+        for c in s:
+            if c in m:
+                if not st or st.pop() != m[c]:
+                    return False
+            else:
+                st.append(c)
+
+        return not st
