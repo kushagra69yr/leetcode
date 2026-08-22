f258f189fed579d115d7b943f4638facd377fd23
Kushagra Burman <kushagraburman21@gmail.com>
2026-08-22 19:05:52 +0530

    Time: 1 ms (71.88%), Space: 14.1 MB (29.46%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,7 +1,6 @@
-class Solution(object):
-    def singleNumber(self, nums):
+class Solution:
+    def singleNumber(self,nums):
         ans = 0
         for n in nums:
             ans=ans^n
         return ans
-        
