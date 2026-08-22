c6aa1b8a01960cf823b4263eccb331d967ea0702
Kushagra Burman <kushagraburman21@gmail.com>
2026-08-22 13:29:55 +0530

    Time: 23 ms (67.34%), Space: 26 MB (22.36%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,3 +1,3 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        return len(nums) != len(set(nums))
+class Solution:
+    def containsDuplicate(self,nums):
+        return len(nums)!=len(set(nums))
