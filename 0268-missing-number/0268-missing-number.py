19ced0bfab33eef8cec9ed51da1f665a0c8663a4
Kushagra Burman <kushagraburman21@gmail.com>
2026-09-14 19:12:45 +0530

    Time: 0 ms (100%), Space: 13.3 MB (37.21%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,4 +1,6 @@
 class Solution:
     def missingNumber(self,nums):
         n = len(nums)
-        return n*(n+1)//2 - sum(nums)
+        total = n*(n+1)//2
+        actual = sum(nums)
+        return total - actual
